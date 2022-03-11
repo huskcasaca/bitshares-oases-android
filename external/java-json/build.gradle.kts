825969dbe5703880ab3813ba41aa71ea707db351
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2022-03-11 15:14:46 +0800

    Added external files

---
+++ external/java-json/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+group = "org.json"
+version = "v20200429-SNAPSHOT"
+description = "JSON in Java"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
